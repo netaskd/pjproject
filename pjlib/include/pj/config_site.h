53fa494b5fe25165798127bd58edbbc8b6d7621e
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-03-05 15:08:57 +0100

    test: skip packets to reduce latency

--- pjlib/include/pj/config_site.h
+++ pjlib/include/pj/config_site.h
@@ -1,3 +1,4 @@
 #define PJMEDIA_HAS_VIDEO   1
 /* Allocate more space for RTCP SDES packets (Linphone uses a quite long name) */
 #define PJMEDIA_RTCP_RX_SDES_BUF_LEN 128
+#define PJMEDIA_VID_STREAM_SKIP_PACKETS_TO_REDUCE_LATENCY 1
